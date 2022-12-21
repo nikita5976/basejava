7798416efd2267a3f74142f4bd5b912707ecc904
Nikita Morozov <nikita-ola@yandex.ru>
2022-12-21 12:49:36 +0500

    решённое задание с исправлением по методу getAll(), защитой от переполнения в save() и исключение угрозы ошибки в delete(), переменная контроля наполнения массива переименована.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,29 +1,33 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  * методы реализованы без проверки uuid на уникальность
  * при добавлении Resume заносится в следующую свободную справа ячейку массива storage
- * защиты от переполнения нет исключение возникнет при добавлении 10001 элемента
- * или удалении из массива заполненного до 10000 ячеек
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    // вел переменную контроля количества внесённых Resume,
+    // вел переменную контроля заполнения массива значениями при пустом массиве значение -1,
     // соответствует значению индекса последней заполненной ячейки массива
-    private int amtResume = -1;
+    private int indResume = -1;
 
     void clear() {
-        for (int i = 0; i <= amtResume; i++) {
+        for (int i = 0; i <= indResume; i++) {
             storage[i] = null;
         }
-        amtResume = -1;
+        indResume = -1;
     }
 
     void save(Resume r) {
-        storage[++amtResume] = r;
+        if (indResume == 9999) {
+            return; //защита от обращения сверх длинны массива
+        } else {
+            storage[++indResume] = r;
+        }
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i <= amtResume; i++) {
+        for (int i = 0; i <= indResume; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
@@ -32,12 +36,16 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        for (int i = 0; i <= amtResume; i++) {
+        for (int i = 0; i <= indResume; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                for (int j = i; j <= amtResume; j++) {
-                    storage[j] = storage[j + 1];// есть риск уехать за 10000
+                for (int j = i; j <= indResume; j++) {
+                    if (j == indResume){   //защита от обращения сверх длинны массива
+                        storage[j]=null;
+                    } else {
+                        storage[j] = storage[j + 1];
+                    }
                 }
-                amtResume--;
+                indResume--;
                 return;
             }
         }
@@ -47,15 +55,11 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] getResume = new Resume[amtResume + 1];
-        for (int i = 0; i <= amtResume; i++) {
-            getResume[i] = storage[i];
-        }
-        return getResume;
+        return Arrays.copyOf(storage, indResume +1);
         //new Resume[0];
     }
 
     int size() {
-        return amtResume + 1;
+        return indResume + 1;
     }
 }
