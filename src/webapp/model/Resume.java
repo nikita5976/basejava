da9f56c469fc186c664b314cc38d976e0a626a9e
Nikita Morozov <nikita-ola@yandex.ru>
2023-02-18 22:21:32 +0500

    Урок 7 добавил Generic изменил Resume

--- src/webapp/model/Resume.java
+++ src/webapp/model/Resume.java
@@ -1,11 +1,16 @@
 package webapp.model;
 
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 
 public class Resume {
     private String uuid;
     private String fullName;
+    private final ObjectivePersonal objective = new ObjectivePersonal();
+    private final ObjectivePersonal personal = new ObjectivePersonal();
+    private final AchievementQualifications achievement = new AchievementQualifications();
+    private final AchievementQualifications qualifications = new AchievementQualifications();
+    private final ExperienceEducation experience = new ExperienceEducation();
+    private final ExperienceEducation education = new ExperienceEducation();
 
     public Resume() {
 
@@ -46,5 +51,76 @@ public class Resume {
         return fullName + " uuid " + uuid;
     }
 
+    public String[] getSection(SectionType section) {
+        return switch (section) {
+            case OBJECTIVE -> objective.getOp();
+            case PERSONAL -> personal.getOp();
+            case ACHIEVEMENT -> achievement.getAq();
+            case QUALIFICATIONS -> qualifications.getAq();
+            case EDUCATION -> education.getEe();
+            case EXPERIENCE -> experience.getEe();
+        };
+    }
 
+    public void setSection(SectionType section, String[] data) {
+        switch (section) {
+            case OBJECTIVE:
+                objective.setOp(data);
+                break;
+            case PERSONAL:
+                personal.setOp(data);
+                break;
+            case ACHIEVEMENT:
+                achievement.setAq(data);
+            case QUALIFICATIONS:
+                qualifications.setAq(data);
+            case EDUCATION:
+                education.setEe(data);
+            case EXPERIENCE:
+                experience.setEe(data);
+        }
+    }
+
+    private final static class ObjectivePersonal {
+        private final StringBuilder op = new StringBuilder();
+
+        private void setOp(String[] inData) {
+            op.append(inData[0]);
+        }
+
+        private String[] getOp() {
+            String[] a = {op.toString()};
+            return a;
+        }
+    }
+
+    private final static class AchievementQualifications {
+        private final LinkedList<String> aq = new LinkedList<>();
+
+        private void setAq(String[] inData) {
+            aq.addLast(inData[0]);
+        }
+
+        private String[] getAq() {
+            String ach = aq.pollFirst();
+            aq.addLast(ach);
+            String[] a = {ach};
+            return a;
+        }
+    }
+
+    private final static class ExperienceEducation {
+
+        private final LinkedList<String[]> ee = new LinkedList<>();
+
+        private void setEe(String[] data) {
+            ee.addLast(data);
+        }
+
+        private String[] getEe() {
+            String[] data = ee.pollFirst();
+            ee.addLast(data);
+            return data;
+        }
+    }
 }
--- src/webapp/storage/AbstractStorage.java
+++ src/webapp/storage/AbstractStorage.java
@@ -6,8 +6,11 @@ import webapp.model.Resume;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
+    //protected final Logger log = Logger.getLogger(getClass().getName());
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     private static final Comparator<Resume> GET_ALL_COMPARATOR = Comparator.comparing(Resume::getFullName)
             .thenComparing(Resume::getUuid);
@@ -15,24 +18,28 @@ public abstract class AbstractStorage<SK> implements Storage {
 
     @Override
     public final void save(Resume r) {
+        LOG.info("Save " + r);
         SK searchKey = getNotExistingSearchKey(r);
         doSave(r, searchKey);
     }
 
     @Override
     public final Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         SK searchKey = getExistingSearchKey(uuid);
         return doGet(searchKey);
     }
 
     @Override
     public final void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         SK searchKey = getExistingSearchKey(uuid);
         doDelete(searchKey);
     }
 
     @Override
     public final List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         List<Resume> allSorted = getAll();
         allSorted.sort(GET_ALL_COMPARATOR);
         return allSorted;
@@ -40,6 +47,7 @@ public abstract class AbstractStorage<SK> implements Storage {
 
     @Override
     public final void update(Resume r) {
+        LOG.info("Update " + r);
         SK searchKey = getExistingSearchKey(r.getUuid());
         doUpdate(searchKey, r);
     }
@@ -48,6 +56,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK getNotExistingSearchKey(Resume r) {
         SK searchKey = getSearchKey(r);
         if (isExist(searchKey)) {
+            LOG.warning("Resume " + r.getUuid() + " not exist");
             throw new ExistStorageException(r.getUuid());
         }
         return searchKey;
@@ -56,6 +65,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK getExistingSearchKey(String uuid) {
         SK searchKey = getSearchKey(uuid);
         if (!isExist(searchKey)) {
+            LOG.warning("Resume " + uuid + " not exist");
             throw new NotExistStorageException(uuid);
         }
         return searchKey;
