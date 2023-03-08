55ace63f7f000b0037d9b09e143a16dd45e59a34
Nikita Morozov <nikita-ola@yandex.ru>
2023-03-08 12:28:47 +0500

    Урок 8.2 FileStorage <File>

--- src/webapp/storage/FileStorage.java
+++ src/webapp/storage/FileStorage.java
@@ -2,8 +2,7 @@ package webapp.storage;
 
 import webapp.model.Resume;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class FileStorage extends AbstractFileStorage {
     protected FileStorage(File directory) {
@@ -12,11 +11,44 @@ public class FileStorage extends AbstractFileStorage {
 
     @Override
     protected void doWrite(Resume r, File file) throws IOException {
+        byte[] byteResume = convertObjectToBytes(r);
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            fos.write(byteResume);
+        }
     }
 
     @Override
     protected Resume doRead(File file) throws IOException {
-        return null;
+        byte[] byteResume;
+        try (FileInputStream fis = new FileInputStream(file)) {
+            int length = fis.available();
+            byteResume = new byte[length];
+            for (int i = 0; i < length; i++) {
+                byteResume[i] = (byte) fis.read();
+            }
+        }
+        return (Resume) convertBytesToObject(byteResume);
+    }
+
+    private byte[] convertObjectToBytes(Object resume) {
+        ByteArrayOutputStream boas = new ByteArrayOutputStream();
+        try (ObjectOutputStream ois = new ObjectOutputStream(boas)) {
+            ois.writeObject(resume);
+            return boas.toByteArray();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        throw new RuntimeException();
+    }
+
+    private Object convertBytesToObject(byte[] bytesResume) {
+        InputStream is = new ByteArrayInputStream(bytesResume);
+        try (ObjectInputStream ois = new ObjectInputStream(is)) {
+            return ois.readObject();
+        } catch (IOException | ClassNotFoundException ioe) {
+            ioe.printStackTrace();
+        }
+        throw new RuntimeException();
     }
 
 }
