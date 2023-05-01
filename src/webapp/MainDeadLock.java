128a28682de535fbfb288018743070b31ecb1663
Nikita Morozov <nikita-ola@yandex.ru>
2023-05-01 19:50:29 +0500

    Урок 11.2 deadlock в MainDeadLock

--- src/webapp/MainDeadLock.java
+++ src/webapp/MainDeadLock.java
@@ -1,56 +1,45 @@
 package webapp;
 
 public class MainDeadLock {
-    static final Object mdl1 =  new Object() {
-        String a = "объект 1";
+    static final MainDeadLock mdl = new MainDeadLock();
+    static final MainDeadLock.TestObject mdl1 = mdl.new TestObject("объект 1");
+    static final MainDeadLock.TestObject mdl2 = mdl.new TestObject("объект 2");
+
+    public static void main(String[] args) {
+
+        System.out.println("в одном потоке " + mdl1 + " & " + mdl2);
+
+        Runnable oneR = () -> {
+            synchronized (mdl1) {
+                System.out.println("два потока и deadlock" + mdl2);
+                synchronized (mdl2) {
+                    System.out.println("stop usage " + mdl2);
+                }
+            }
+        };
+
+        Runnable twoR = () -> {
+            synchronized (mdl2) {
+                System.out.println("два потока и deadlock" + mdl1);
+                synchronized (mdl1) {
+                    System.out.println("stop usage " + mdl1);
+                }
+            }
+        };
+        new Thread(oneR).start();
+        new Thread(twoR).start();
+    }
+
+    private  class TestObject {
+        String a;
+        TestObject (String a) {
+            this.a = a;
+        }
+
         @Override
         public String toString(){
             return a;
         }
-    };
-    static final Object mdl2 = new Object(){
-        String b = "объект 2";
-        @Override
-        public String toString(){
-            return b;
-        }
-    };
-
-    public static void main(String[] args) {
-
-      System.out.println("в одном потоке "+mdl1);
-      System.out.println("в одном потоке "+mdl2);
-
-       Runnable oneR = () -> {
-           synchronized (mdl1){
-               System.out.println("два потока и deadlock"+mdl2);
-               try {
-                   Thread.sleep(200);
-               } catch (InterruptedException exception) {
-                   System.out.println(exception);
-               }
-//               Thread.yield();
-               synchronized (mdl2) {
-                   System.out.println("stop usage "+mdl2);
-               }
-           }
-       };
-
-       Runnable twoR = ()-> {
-           synchronized (mdl2){
-               System.out.println("два потока и deadlock"+mdl1);
-               try {
-                   Thread.sleep(200);
-               } catch (InterruptedException exception) {
-                   System.out.println(exception);
-               }
- //              Thread.yield();
-               synchronized (mdl1) {
-                   System.out.println("stop usage "+mdl1);
-               }
-           }
-       };
-       new Thread(oneR).start();
-       new Thread(twoR).start();
     }
+
 }
