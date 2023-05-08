3302f50157ae2af9c926f40fbc683a1f0e044760
Nikita Morozov <nikita-ola@yandex.ru>
2023-05-08 10:32:12 +0500

    Урок 12.1 оптимизировано

--- src/webapp/MyStreams.java
+++ src/webapp/MyStreams.java
@@ -2,8 +2,9 @@ package webapp;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class MyStreams {
     public static void main(String[] args) {
@@ -11,12 +12,13 @@ public class MyStreams {
         int[] array = {1, 4, 5, 9, 1, 6, 9, 4, 4, 5};
         System.out.println(minValue(array));
 
-
+        int sumValue = 0;
         List<Integer> integers = new ArrayList<>();
         for (int i = 0; i < 1000002; i++) {
             integers.add(i);
+            sumValue = sumValue + i;
         }
-        //System.out.println(oddOrEven(integers));
+
         List<Integer> test = oddOrEven(integers);
         int even = 0;
         int odd = 0;
@@ -25,37 +27,33 @@ public class MyStreams {
                 even++;
             } else odd++;
         }
-        System.out.println("Сумма элементов " + utilVarForOddOrEven);
+        System.out.println("Сумма элементов   " + sumValue);
         System.out.println("чётных - " + even);
         System.out.println("не чётных - " + odd);
 
     }
 
-    private static volatile int utilVarForMinValue;
-
     private static int minValue(int[] values) {
-        MyStreams.utilVarForMinValue = 1;
         return Arrays.stream(values)
-                .boxed()
-                .sorted(Collections.reverseOrder())
-                .mapToInt(Integer::intValue)
                 .distinct()
-                .map((value) -> value * (utilVarForMinValue = utilVarForMinValue * 10))
-                .sum() / 10;
+                .sorted()
+                .reduce(0, (a, b) -> {
+                    a = a * 10 + b;
+                    return a;
+                });
     }
 
-    private static volatile int utilVarForOddOrEven;
-
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        MyStreams.utilVarForOddOrEven = 0;
-        return integers.stream()
-                .peek(integer -> utilVarForOddOrEven = utilVarForOddOrEven + integer)
-                .sorted()
-                .filter(integer -> {
-                    if (utilVarForOddOrEven % 2 != 0)
-                        return integer % 2 == 0;
-                    else return integer % 2 != 0;
-                })
+        Supplier<Stream<Integer>> streamSupplier = integers::stream;
+
+        final boolean evenSum = streamSupplier.get()
+                .reduce(0, (sum, v) -> {
+                    sum = sum + v;
+                    return sum;
+                }) % 2 == 0;
+
+        return streamSupplier.get()
+                .filter(integer -> evenSum == (integer % 2 != 0))
                 .toList();
     }
 }
