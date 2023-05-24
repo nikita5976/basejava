ba8419ff42d24bc092d4f9ade181e8ef9c141476
Nikita Morozov <nikita-ola@yandex.ru>
2023-05-24 22:11:53 +0500

    Урок 13 step 3 SqlHelper ok

--- src/webapp/storage/SqlStorage.java
+++ src/webapp/storage/SqlStorage.java
@@ -3,130 +3,110 @@ package webapp.storage;
 import org.postgresql.util.PSQLException;
 import webapp.exception.ExistStorageException;
 import webapp.exception.NotExistStorageException;
-import webapp.exception.StorageException;
 import webapp.model.Resume;
 import webapp.sql.ConnectionFactory;
+import webapp.sql.SqlHelper;
 
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SqlStorage implements Storage {
 
     public final ConnectionFactory connectionFactory;
+    SqlHelper sqlHelper;
 
     public SqlStorage(String dbUrl, String dbUser, String dbPassword) {
         connectionFactory = () -> DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        this.sqlHelper = new SqlHelper(connectionFactory);
     }
 
     @Override
     public void clear() {
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement("DELETE FROM resume")) {
+        String clearSQL = "DELETE FROM resume";
+        sqlHelper.usePreparedStatement(clearSQL, ps -> {
             ps.execute();
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+            return null;
+        });
     }
 
     @Override
     public Resume get(String uuid) {
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement("SELECT * FROM resume r WHERE r.uuid =?")) {
+        String getSQL = "SELECT * FROM resume r WHERE r.uuid =?";
+        return sqlHelper.usePreparedStatement(getSQL, ps -> {
             ps.setString(1, uuid);
             ResultSet rs = ps.executeQuery();
             if (!rs.next()) {
                 throw new NotExistStorageException(uuid);
             }
             return new Resume(uuid, rs.getString("full_name"));
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+        });
     }
 
     @Override
     public void update(Resume r) {
         final String updateSQL = "UPDATE resume SET full_name = ? WHERE uuid = ?;";
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement(updateSQL)) {
+        sqlHelper.usePreparedStatement(updateSQL, ps -> {
             ps.setString(1, r.getFullName());
             ps.setString(2, r.getUuid());
             int rs = ps.executeUpdate();
             if (rs == 0) {
                 throw new NotExistStorageException(r.getUuid());
             }
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+            return null;
+        });
     }
 
     @Override
     public void save(Resume r) {
         final String saveSQL = "INSERT INTO resume (uuid, full_name) VALUES (?,?)";
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement(saveSQL)) {
-
-            ps.setString(1, r.getUuid());
-            ps.setString(2, r.getFullName());
-            ps.execute();
-        } catch (PSQLException e) { //есть сомнения может стовить разное
-            throw new ExistStorageException(r.getUuid(), e);
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
-
+        sqlHelper.usePreparedStatement(saveSQL, ps -> {
+            try {
+                ps.setString(1, r.getUuid());
+                ps.setString(2, r.getFullName());
+                ps.execute();
+            } catch (PSQLException e) { //есть сомнения может выбросится не только если повтор
+                throw new ExistStorageException(r.getUuid(), e);
+            }
+            return null;
+        });
     }
 
     @Override
     public void delete(String uuid) {
         final String deleteSQL = "DELETE FROM resume WHERE uuid = ?;";
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement(deleteSQL)) {
-            ps.setString(1,uuid);
+        sqlHelper.usePreparedStatement(deleteSQL, ps -> {
+            ps.setString(1, uuid);
             int rs = ps.executeUpdate();
-
             if (rs == 0) {
                 throw new NotExistStorageException(uuid);
             }
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+            return null;
+        });
     }
 
     @Override
     public List<Resume> getAllSorted() {
         final String getAllSortedSQL = "SELECT * FROM resume;";
-        List <Resume> listResume = new ArrayList<>();
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement(getAllSortedSQL)) {
-
+        List<Resume> listResume = new ArrayList<>();
+        return sqlHelper.usePreparedStatement(getAllSortedSQL, ps -> {
             ResultSet rs = ps.executeQuery();
-            while (rs.next()){
+            while (rs.next()) {
                 Resume resume = new Resume(rs.getString("uuid"), rs.getString("full_name"));
                 listResume.add(resume);
             }
-
             return listResume;
-
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+        });
     }
 
     @Override
     public int size() {
-        ResultSet rs;
-        int size;
         final String sizeSQL = "SELECT COUNT(*) FROM resume ;";
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sizeSQL)) {
-            rs = ps.executeQuery();
+        return sqlHelper.usePreparedStatement(sizeSQL, ps -> {
+            ResultSet rs = ps.executeQuery();
             rs.next();
-            size = rs.getInt(1);
-
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
-        return size;
+            return rs.getInt(1);
+        });
     }
 }
